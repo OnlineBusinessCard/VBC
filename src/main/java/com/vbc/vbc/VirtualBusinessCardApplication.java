f0b554db914cd3aa0126e517f8f1cf40a4f6541b
michael garrison <mgarrison.sa@gmail.com>
2020-09-18 17:02:44 -0500

    project setup

---
+++ src/main/java/com/vbc/vbc/VirtualBusinessCardApplication.java
@@ -0,0 +1,13 @@
+package com.vbc.vbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualBusinessCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualBusinessCardApplication.class, args);
+    }
+
+}
