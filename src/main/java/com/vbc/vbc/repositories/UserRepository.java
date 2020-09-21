7cea3b16642bfaf123da3a3d867fc52d1d93c8fd
michael garrison <mgarrison.sa@gmail.com>
2020-09-20 21:48:34 -0500

    user repository

---
+++ src/main/java/com/vbc/vbc/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.vbc.vbc.repositories;
+
+import com.vbc.vbc.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+}
