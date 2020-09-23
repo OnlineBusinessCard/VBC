12e9ca8a400b937a7b2766bc0af5c456ee3098e2
michael garrison <mgarrison.sa@gmail.com>
2020-09-22 23:25:58 -0500

    business card model v1

---
+++ src/main/java/com/vbc/vbc/models/Card.java
@@ -0,0 +1,76 @@
+package com.vbc.vbc.models;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "cards")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(nullable = false, length = 50)
+    private String website;
+
+    @Column
+    private String phone;
+
+    @OneToOne(cascade = CascadeType.MERGE, mappedBy = "userCard")
+    private User user;
+
+    public Card() {
+    }
+
+    public Card(long id, String title, String website, String phone, User user){
+        this.id = id;
+        this.title = title;
+        this.website = website;
+        this.phone = phone;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
