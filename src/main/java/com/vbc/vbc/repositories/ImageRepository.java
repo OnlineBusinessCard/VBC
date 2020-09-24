9cb194a2a1a07a48ed4678348bde063641e196ce
michael garrison <mgarrison.sa@gmail.com>
2020-09-23 19:57:34 -0500

    image repository

---
+++ src/main/java/com/vbc/vbc/repositories/ImageRepository.java
@@ -0,0 +1,8 @@
+package com.vbc.vbc.repositories;
+
+import com.vbc.vbc.models.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
