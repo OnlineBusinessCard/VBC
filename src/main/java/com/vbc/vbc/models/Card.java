3d80e4f63cd9b4bfb28192eea8d0a69a504e60c6
michael garrison <mgarrison.sa@gmail.com>
2020-09-28 20:22:51 -0500

    updated card model

--- src/main/java/com/vbc/vbc/models/Card.java
+++ src/main/java/com/vbc/vbc/models/Card.java
@@ -48,13 +48,21 @@ public class Card {
     public Card() {
     }
 
-    public Card(long id, String title, String website, String phone, User user){
+    public Card(long id, String title, String website, String phone, User user, String firstName, String lastName, String email, String company, String city, String state, String country){
         this.id = id;
         this.title = title;
         this.website = website;
         this.phone = phone;
         this.user = user;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.company = company;
+        this.city = city;
+        this.state = state;
+        this.country = country;
     }
+
 
     public long getId() {
         return id;
@@ -62,6 +70,62 @@ public class Card {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
     public String getTitle() {
