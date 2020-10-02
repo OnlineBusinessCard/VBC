6ec5a3485c2d294dfdf90d07abbfc55789300b5c
michael garrison <mgarrison.sa@gmail.com>
2020-10-01 21:19:42 -0500

    working on adding image for user profile

--- src/main/java/com/vbc/vbc/controllers/UserController.java
+++ src/main/java/com/vbc/vbc/controllers/UserController.java
@@ -1,27 +1,29 @@
 package com.vbc.vbc.controllers;
 
+import com.vbc.vbc.models.Image;
 import com.vbc.vbc.models.User;
+import com.vbc.vbc.repositories.ImageRepository;
 import com.vbc.vbc.repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class UserController {
 
     private UserRepository userDao;
     private PasswordEncoder passwordEncoder;
+    private ImageRepository imagesDao;
 
 
-    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder, ImageRepository imagesDao) {
         this.userDao = userDao;
         this.passwordEncoder = passwordEncoder;
+        this.imagesDao = imagesDao;
     }
 
     @GetMapping("/register")
@@ -42,6 +44,8 @@ public class UserController {
     public String userDashboard(Model model){
         User sessionUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = userDao.getOne(sessionUser.getId());
+        List<Image> myImage = imagesDao.findAll();
+        model.addAttribute("images", myImage);
         model.addAttribute("user", user);
         return "users/dashboard";
     }
@@ -62,12 +66,16 @@ public class UserController {
     }
 
     @PostMapping("/profile/edit/{id}")
-    public String update(@PathVariable long id, @ModelAttribute User user){
+    public String update(@PathVariable long id, @ModelAttribute User user, @RequestParam String imageUpload){
         User currentUser = userDao.findById(id);
+        Image img = new Image();
+        img.setFilestackUrl(imageUpload);
+        img.setUser(user);
         currentUser.setFirstName(user.getFirstName());
         currentUser.setLastName(user.getLastName());
         currentUser.setUsername(user.getUsername());
         currentUser.setEmail(user.getEmail());
+        imagesDao.save(img);
         userDao.save(currentUser);
         return "redirect:/dashboard";
     }
--- src/main/java/com/vbc/vbc/repositories/ImageRepository.java
+++ src/main/java/com/vbc/vbc/repositories/ImageRepository.java
@@ -1,8 +1,15 @@
 package com.vbc.vbc.repositories;
 
 import com.vbc.vbc.models.Image;
+import com.vbc.vbc.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    @Query("SELECT u FROM Image u WHERE u.user = ?1")
+    List<Image> findImageById(User Id);
 
 }
