058e1cb4864f8d844eaef54f46adeb5f65d9c984
michael garrison <mgarrison.sa@gmail.com>
2020-10-02 17:34:05 -0500

    migration file

---
+++ database/migration.sql
@@ -0,0 +1,45 @@
+DROP DATABASE IF EXISTS vbc_db;
+CREATE DATABASE IF NOT EXISTS vbc_db;
+USE vbc_db;
+
+SHOW TABLES;
+
+DESCRIBE users;
+DESCRIBE cards;
+DESCRIBE images;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    card_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (username, email),
+    FOREIGN KEY (card_id) REFERENCES cards (id)
+);
+
+CREATE TABLE IF NOT EXISTS cards (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(50) NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    website VARCHAR(50) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    state VARCHAR(50) NOT NULL,
+    country VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (email)
+);
+
+CREATE TABLE IF NOT EXISTS images (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    filestack_url VARCHAR(255) NOT NULL ,
+    user_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
