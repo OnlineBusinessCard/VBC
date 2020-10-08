84ef8ab804b6b667d7aa29b708bfabb5bc8e56ec
michael garrison <mgarrison.sa@gmail.com>
2020-10-07 23:05:51 -0500

    refactored lead repository to find leads by user

--- src/main/java/com/vbc/vbc/repositories/LeadRepository.java
+++ src/main/java/com/vbc/vbc/repositories/LeadRepository.java
@@ -2,7 +2,13 @@ package com.vbc.vbc.repositories;
 
 import com.vbc.vbc.models.Lead;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LeadRepository extends JpaRepository<Lead, Long> {
+
+    @Query("from Lead l where l.user.id = ?1")
+    List<Lead> findAllLeadsByAuthor(long userId);
 
 }
