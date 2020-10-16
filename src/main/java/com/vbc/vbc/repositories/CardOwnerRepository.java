e1265121181f96237a18cb1d4a603e8bce337f1e
michael garrison <mgarrison.sa@gmail.com>
2020-10-16 18:03:17 -0500

    created cardowner repository

---
+++ src/main/java/com/vbc/vbc/repositories/CardOwnerRepository.java
@@ -0,0 +1,8 @@
+package com.vbc.vbc.repositories;
+
+import com.vbc.vbc.models.CardOwner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CardOwnerRepository extends JpaRepository<CardOwner, Long> {
+
+}
