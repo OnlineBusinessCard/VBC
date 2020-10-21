754e76a64fe364766b5cfe1fcc44bd85f36a6229
michael garrison <mgarrison.sa@gmail.com>
2020-10-20 19:40:55 -0500

    refactored card controller with card owner dao/refactored

--- src/main/java/com/vbc/vbc/controllers/CardController.java
+++ src/main/java/com/vbc/vbc/controllers/CardController.java
@@ -1,7 +1,9 @@
 package com.vbc.vbc.controllers;
 
 import com.vbc.vbc.models.Card;
+import com.vbc.vbc.models.CardOwner;
 import com.vbc.vbc.models.User;
+import com.vbc.vbc.repositories.CardOwnerRepository;
 import com.vbc.vbc.repositories.CardRepository;
 import com.vbc.vbc.repositories.ImageRepository;
 import com.vbc.vbc.repositories.UserRepository;
@@ -10,17 +12,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.security.PermitAll;
-
 @Controller
 public class CardController {
 
+    private final CardOwnerRepository cardOwnerDao;
     private final CardRepository cardsDao;
     private final UserRepository usersDao;
     private final ImageRepository imageDao;
 
 
-    public CardController(CardRepository cardsDao, UserRepository usersDao, ImageRepository imageDao) {
+    public CardController(CardOwnerRepository cardOwnerDao, CardRepository cardsDao, UserRepository usersDao, ImageRepository imageDao) {
+        this.cardOwnerDao = cardOwnerDao;
         this.cardsDao = cardsDao;
         this.usersDao = usersDao;
         this.imageDao = imageDao;
@@ -46,7 +48,7 @@ public class CardController {
     @PostMapping("/card/create")
     public String createCard(@ModelAttribute Card card, @RequestParam String imageUpload){
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        user.setCard(user.getCard());
+        card.setCardOwner(user.getCardOwner());
         cardsDao.save(card);
         return "redirect:/card/{id}";
     }
@@ -61,8 +63,8 @@ public class CardController {
     //EDIT CARD
     @PostMapping("/card/{id}/edit")
     public String editCard(@PathVariable long id, @ModelAttribute Card card) {
-        User user = usersDao.getOne(1L);
-        card.setUser(user);
+        CardOwner user = cardOwnerDao.getOne(1L);
+        card.setCardOwner(user);
         cardsDao.save(card);
         return "redirect:/card/view";
     }
