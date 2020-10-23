5cb3074308e7363308e334db461a9f758a6e007b
michael garrison <mgarrison.sa@gmail.com>
2020-10-22 23:09:20 -0500

    refactored navbar/card create view - updated review repository

--- src/main/java/com/vbc/vbc/repositories/ReviewRepository.java
+++ src/main/java/com/vbc/vbc/repositories/ReviewRepository.java
@@ -2,7 +2,16 @@ package com.vbc.vbc.repositories;
 
 import com.vbc.vbc.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @Query("from Review r where r.cardOwner.id = ?1")
+    List<Review> findAllReviewsByCardOwner(long cardOwnerId);
+
+    @Query("from Review r where r.author.id = ?1")
+    List<Review> findAllReviewsByAuthor(long userId);
 
 }
