c650ceb4ae3512f0b320bddda00d6258d8579c61
michael garrison <mgarrison.sa@gmail.com>
2020-10-27 21:18:45 -0500

    added ability to find all card owners

--- src/main/java/com/vbc/vbc/repositories/CardRepository.java
+++ src/main/java/com/vbc/vbc/repositories/CardRepository.java
@@ -1,8 +1,12 @@
 package com.vbc.vbc.repositories;
 
 import com.vbc.vbc.models.Card;
+import com.vbc.vbc.models.CardOwner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CardRepository extends JpaRepository<Card, Long> {
+    List<Card> findAllByCardOwner(CardOwner cardOwner);
 
 }
